40f65a47ce576f7bf9ade87b77bdaad7d4e3fa80
dogmax296 <dogmax296@DESKTOP-R06I8M6>
2024-08-07 15:20:13 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AccessControlApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
